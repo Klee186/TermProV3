b380dfcddd4bf531cf327006c040fef91b04927c
Klee186 <157643058+Klee186@users.noreply.github.com>
2024-04-21 22:41:19 -0400

    Update app.js

--- TermProV3/app.js
+++ TermProV3/app.js
@@ -1,1 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/feedback', (req, res) => {
+  res.sendFile(__dirname + '/feedback.html');
+});
+
+app.post('/feedback-submit', (req, res) => {
+  const likes = req.body.likes;
+  const suggestions = req.body.suggestions;
+
+  res.send(`Thank you for your feedback!<br>Likes: ${likes}<br>Suggestions: ${suggestions}`);
+});
+
+app.post('/toggle-sidebar', (req, res) => {
+
+  res.send('Sidebar toggled successfully');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
